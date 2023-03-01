792d00999e51dd00a5e4d3cbb86a1dd90316bc19
mahlet <akotet2123@gmail.com>
2023-03-01 15:55:08 -0800

    height of a tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,12 +16,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	else if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	else
-	{
-		left_side = binary_tree_height(tree->left);
-		right_side = binary_tree_height(tree->right);
-	}
-	if(left_side >= right_side)
+	left_side = binary_tree_height(tree->left);
+	right_side = binary_tree_height(tree->right);
+	if (left_side >= right_side)
 		return (left_side + 1);
 	else
 		return (right_side + 1);
